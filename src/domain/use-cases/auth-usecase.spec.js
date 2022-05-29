796430dcb49230726d001a9c6cdbd8d8a48f14fc
Anna Lopes <anna.lopes@dtidigital.com.br>
2022-05-29 11:24:44 -0300

    ensure AuthUseCase calls Encrypter with correct values

--- src/domain/use-cases/auth-usecase.spec.js
+++ src/domain/use-cases/auth-usecase.spec.js
@@ -2,18 +2,28 @@ const { MissingParamError, InvalidParamError } = require('../../utils/errors')
 const AuthUseCase = require('./auth-usecase')
 
 const makeSut = () => {
+  class EncrypterSpy {
+    async compare (password, hashedPassword) {
+      this.password = password
+      this.hashedPassword = hashedPassword
+    }
+  }
+
+  const encrypterSpy = new EncrypterSpy()
+
   class LoadUserByEmailRepositorySpy {
     async load (email) {
       this.email = email
       return this.user
     }
   }
+
   const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
-  loadUserByEmailRepositorySpy.user = {}
+  loadUserByEmailRepositorySpy.user = { password: 'hashed_password' }
 
-  const sut = new AuthUseCase(loadUserByEmailRepositorySpy)
+  const sut = new AuthUseCase(loadUserByEmailRepositorySpy, encrypterSpy)
 
-  return { sut, loadUserByEmailRepositorySpy }
+  return { sut, loadUserByEmailRepositorySpy, encrypterSpy }
 }
 
 describe('Auth UseCase', () => {
@@ -56,6 +66,7 @@ describe('Auth UseCase', () => {
     const { sut, loadUserByEmailRepositorySpy } = makeSut()
     loadUserByEmailRepositorySpy.user = null
     const acessToken = await sut.auth('invalid_email@mail.com', 'any_password')
+
     expect(acessToken).toBe(null)
   })
 
@@ -64,5 +75,13 @@ describe('Auth UseCase', () => {
     const acessToken = await sut.auth('valid_email@mail.com', 'invalid_password')
 
     expect(acessToken).toBe(null)
+  })
+
+  test('Should call Encrypter with correct value', async () => {
+    const { sut, loadUserByEmailRepositorySpy, encrypterSpy } = makeSut()
+    await sut.auth('valid_email@mail.com', 'any_password')
+
+    expect(encrypterSpy.password).toBe('any_password')
+    expect(encrypterSpy.hashedPassword).toBe(loadUserByEmailRepositorySpy.user.password)
   })
 })
