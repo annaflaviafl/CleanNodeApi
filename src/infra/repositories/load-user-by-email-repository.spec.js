5620fa510dd5f190834529cb57d7938a5bb9c554
Anna Lopes <anna.lopes@dtidigital.com.br>
2022-05-30 20:31:38 -0300

    ensure LoadUserByEmailRepository returns an user with id only

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,51 +1,63 @@
-const { MongoClient } = require('mongodb')
-let client, db
+const { MongoClient } = require("mongodb");
+let client, db;
 class LoadUserByEmailRepository {
-  constructor (userModel) {
-    this.userModel = userModel
+  constructor(userModel) {
+    this.userModel = userModel;
   }
 
-  async load (email) {
-    const user = this.userModel.findOne({ email })
-    return user
+  async load(email) {
+    const user = this.userModel.findOne(
+      { email },
+      {
+        projection: {
+          password: 1,
+        },
+      }
+    );
+    return user;
   }
 }
 
 const makeSut = () => {
-  const userModel = db.collection('users')
-  const sut = new LoadUserByEmailRepository(userModel)
+  const userModel = db.collection("users");
+  const sut = new LoadUserByEmailRepository(userModel);
 
-  return { sut, userModel }
-}
-describe('LoadUserByEmail Repository', () => {
+  return { userModel, sut };
+};
+describe("LoadUserByEmail Repository", () => {
   beforeAll(async () => {
     client = await MongoClient.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
-    db = client.db()
-  })
+      useUnifiedTopology: true,
+    });
+    db = client.db();
+  });
 
   beforeEach(async () => {
-    await db.collection('users').deleteMany()
-  })
+    await db.collection("users").deleteMany();
+  });
 
   afterAll(async () => {
-    await client.close()
-  })
+    await client.close();
+  });
 
-  test('Should return null if no user is found', async () => {
-    const { sut } = makeSut()
-    const user = await sut.load('invalid_email@mail.com')
-    expect(user).toBeNull()
-  })
+  test("Should return null if no user is found", async () => {
+    const { sut } = makeSut();
+    const user = await sut.load("invalid_email@mail.com");
+    expect(user).toBeNull();
+  });
 
-  test('Should return an user if user is found', async () => {
-    const { sut, userModel } = makeSut()
-    await userModel.insertOne({
-      email: 'valid_email@mail.com'
-    })
-    const user = await sut.load('valid_email@mail.com')
-    expect(user.email).toBe('valid_email@mail.com')
-  })
-})
+  test("Should return an user if user is found", async () => {
+    const { sut, userModel } = makeSut();
+    const fakeUser = await userModel.insertOne({
+      email: "valid_email@mail.com",
+      password: "hashed_password",
+    });
+
+    const user = await sut.load("valid_email@mail.com");
+    expect(user).toEqual({
+      _id: fakeUser.insertedId,
+      password: "hashed_password",
+    });
+  });
+});
