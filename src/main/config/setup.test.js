8987c37d6a3bd0392bd04800483b5151d4a0e552
Anna Lopes <anna.lopes@dtidigital.com.br>
2022-05-31 07:58:28 -0300

    move test cors to its own file

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -9,14 +9,4 @@ describe("App setup", () => {
     const res = await request(app).get("/test_x_powered_by");
     expect(res.headers["x-powered-by"]).toBeUndefined();
   });
-
-  test("Should enable CORS", async () => {
-    app.get("/test_cors", (req, res) => {
-      res.send("");
-    });
-    const res = await request(app).get("/test_cors");
-    expect(res.headers["acess-control-allow-origin"]).toBe("*");
-    expect(res.headers["acess-control-allow-methods"]).toBe("*");
-    expect(res.headers["acess-control-allow-headers"]).toBe("*");
-  });
 });
