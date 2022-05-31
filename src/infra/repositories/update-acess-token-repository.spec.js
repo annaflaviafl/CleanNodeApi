d6922a5a8ebe0ec20c8697ad6256a3d8e617e433
Anna Lopes <anna.lopes@dtidigital.com.br>
2022-05-30 21:23:00 -0300

    ensure UpdateAcessTokenRepository updates user with the given acessToken

---
+++ src/infra/repositories/update-acess-token-repository.spec.js
@@ -0,0 +1,47 @@
+const MongoHelper = require("../helpers/mongo-helper");
+let db;
+
+class UpdateAcessTokenRepository {
+  constructor(userModel) {
+    this.userModel = userModel;
+  }
+  async update(userId, acessToken) {
+    await this.userModel.updateOne(
+      {
+        _id: userId,
+      },
+      {
+        $set: { acessToken },
+      }
+    );
+  }
+}
+
+describe("UpdateAcessToken Repository", () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+    db = await MongoHelper.db;
+  });
+
+  beforeEach(async () => {
+    await db.collection("users").deleteMany();
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  test("Should update the user with the given acessToken", async () => {
+    const userModel = db.collection("users");
+    const sut = new UpdateAcessTokenRepository(userModel);
+    const fakeUser = await userModel.insertOne({
+      email: "valid_email@mail.com",
+      password: "hashed_password",
+    });
+    await sut.update(fakeUser.insertedId, "valid_token");
+    const updateFakeUser = await userModel.findOne({
+      _id: fakeUser.insertedId,
+    });
+    expect(updateFakeUser.acessToken).toBe("valid_token");
+  });
+});
