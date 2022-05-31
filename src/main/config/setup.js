fb720694c1cab22ade3cf3d6946f5abd21f814bd
Anna Lopes <anna.lopes@dtidigital.com.br>
2022-05-31 08:08:03 -0300

    ensure requests parse body as JSON

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,6 +1,8 @@
 const cors = require("../middlewares/cors");
+const jsonParse = require("../middlewares/json-parser");
 
 module.exports = (app) => {
   app.disable("x-powered-by");
   app.use(cors);
+  app.use(jsonParse);
 };
