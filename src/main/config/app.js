9911a2108293b153bd0e7b023ed73342b58a65e4
Anna Lopes <anna.lopes@dtidigital.com.br>
2022-05-31 21:44:55 -0300

    add routes file

--- src/main/config/app.js
+++ src/main/config/app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const { set } = require("express/lib/application");
 const app = express();
 const setupApp = require("./setup");
+const setupRoutes = require("./routes");
 
 setupApp(app);
+setupRoutes(app);
 
 module.exports = app;
