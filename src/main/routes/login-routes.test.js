7597563cf441c085246ae99dd6bb289bfd11d774
Anna Lopes <anna.lopes@dtidigital.com.br>
2022-06-02 22:05:43 -0300

    add login route integration test to ensure 401 response when invalid credentials are provided

--- src/main/routes/login-routes.test.js
+++ src/main/routes/login-routes.test.js
@@ -27,4 +27,11 @@ describe("Login Routes", () => {
       .send({ email: "valid_email@mail.com", password: "hashed_password" })
       .expect(200);
   });
+
+  test("Should return 401 when invalid credentials are provided", async () => {
+    await request(app)
+      .post("/api/login")
+      .send({ email: "valid_email@mail.com", password: "hashed_password" })
+      .expect(401);
+  });
 });
